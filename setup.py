b178995909e27027dcbf409abffebe2c7ed90576
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-05-17 10:29:15 +0200

    actualizaciones 17_05_2025
    Correción en los archivo __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fletplus",
-    version="0.1.0",
+    version="0.2.0",
     author="Adolfo González Hernández",
     author_email="adolfogonzal@gmail.com",
     description="Componentes avanzados y utilidades para apps Flet en Python",
