87b6d914552ed4b1a4b2083c31b2d3565e856863
Adolfo González Hernández <adolfogonzal@gmail.com>
2026-02-07 12:47:01 +0100

    Update core layout exports and docstrings

--- fletplus/core/__init__.py
+++ fletplus/core/__init__.py
@@ -1,15 +1,13 @@
 """Núcleo desacoplado de FletPlus."""
 
 from .app import FletPlusApp
-from .layout import Layout, LayoutBuilder, LayoutComposition, LayoutUpdater
+from .layout import Layout, LayoutComposition
 from .state import AppState, StateProtocol
 
 __all__ = [
     "AppState",
     "FletPlusApp",
     "Layout",
-    "LayoutBuilder",
     "LayoutComposition",
-    "LayoutUpdater",
     "StateProtocol",
 ]
